cc066ef6c50cdee29ffc98baf9a8bb5e5dce21e6
Richard Apgar <apgar.richie05@yahoo.com>
2024-03-18 15:44:00 -0400

    Database created
    Problems when using the command 'Update-Database' in the package manager console

---
+++ BotTest/DbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BotContext : DbContext
+{
+    public DbSet<Profile> Profile { get; set; }
+    public DbSet<UserList> List { get; set; }
+
+    public string DbPath { get; }
+    public BotContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "bot.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class UserList
+{
+    public string UserListId { get; set; }
+    public List<Profile> Profiles { get; set; }
+}
+
+public class Profile
+{
+    public string ProfileId { get; set; }
+    public string Name { get; set; }
+    public ulong DiscordId { get; set; }
+    public int Money { get; set; }
+    public int Level { get; set; }
+    public int Experience { get; set; }
+    public int InventorySpace { get; set; }
+}

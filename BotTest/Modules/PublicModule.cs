db94c62bbc41fe994d361f1ccef1a8cf82a7c869
Vincenzo Capra <vcapra71@gmail.com>
2024-03-19 13:05:26 -0400

    Im messing up my file placement smh!
    Use this build as a backup for the most recent one!

--- BotTest/Modules/PublicModule.cs
+++ BotTest/Modules/PublicModule.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Discord;
 using Discord.Commands;
 using System.IO;
@@ -60,66 +59,3 @@ public class PublicModule : ModuleBase<SocketCommandContext>
         await ReplyAsync(user.ToString());
     }
 }
-=======
-using Discord;
-using Discord.Commands;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using TextCommandFramework.Services;
-
-namespace TextCommandFramework.Modules;
-
-// Modules must be public and inherit from an IModuleBase
-public class PublicModule : ModuleBase<SocketCommandContext>
-{
-    private readonly BotContext _db;
-
-    public PublicModule(BotContext db)
-    {
-        _db = db;
-    }
-
-    [Command("new")]
-    public async Task NewAsync()
-    {
-        var user = await _db.Profile.FirstOrDefaultAsync(user => user.DiscordId == Context.User.Id);
-
-        if (user != null)
-        {
-            await ReplyAsync("You already have a profile!");
-            return;
-        }
-        else
-        {
-            var profile = new Profile
-            {
-                Name = Context.User.GlobalName,
-                DiscordId = Context.User.Id,
-                Money = 100,
-                Level = 1,
-                Experience = 0,
-                InventorySpace = 10
-            };
-
-            _db.Profile.Add(profile);
-            await _db.SaveChangesAsync();
-            await ReplyAsync("Account created!");
-        }
-    }
-
-    [Command("ping")]
-    public Task PingAsync()
-        => ReplyAsync("pong!");
-
-    // Get info on a user, or the user who invoked the command if one is not specified
-    [Command("userinfo")]
-    public async Task UserInfoAsync(IUser user = null)
-    {
-        user ??= Context.User;
-
-        await ReplyAsync(user.ToString());
-    }
-}
->>>>>>> 1536924105a07e5db60238e5b76e23b63bc2d460

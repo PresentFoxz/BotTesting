275c841247e751114214a74ee20daa4b125b4099
Vincenzo Capra <vcapra71@gmail.com>
2024-03-21 11:11:08 -0400

    Fix

--- BotTest/Modules/PublicModule.cs
+++ BotTest/Modules/PublicModule.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore.Design;
 using TextCommandFramework.Services;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Collections.Generic;
+using System;
 
 namespace TextCommandFramework.Modules;
 
@@ -28,7 +29,9 @@ public class PublicModule : ModuleBase<SocketCommandContext>
         Profile profile = user;
 
         // 0: Chicken, 1: Bee, 2: Poisonous Spider, 3: Wolf
-        List<int> dungeonCrawlers = new List<int> {};
+        List<int> dungeonCrawlers = new List<int> {1, 3, 3, 5};
+        Random rnd = new Random();
+        int fight = -1;
 
         if (mess1 == "account")
         {
@@ -76,10 +79,44 @@ public class PublicModule : ModuleBase<SocketCommandContext>
             }
         }
 
+        if (mess1 == "inventory")
+        {
+            if (mess2 == "checkInv")
+            {
+                return;
+            }
+        }
+
         if (mess1 == "Dungeon")
         {
-            if (user != null && mess2 == "crawl")
+            if (user != null && mess2 == "crawl" && fight < 0)
             {
+                int move = rnd.Next(1, 30);
+
+                if (move > 20)
+                {
+                    fight = rnd.Next(0, 3);
+                }
+                else
+                {
+                    return;
+                }
+
+                return;
+            }
+            else if (user != null && mess2 == "crawl" && fight >= 0)
+            {
+                await ReplyAsync("You're in a fight! --> !Game dungeon fight");
+                return;
+            }
+
+            if (user != null && mess2 == "fight" && fight >= 0)
+            {
+
+            }
+            else
+            {
+                await ReplyAsync("You just swung at mid air like a crazy man! Are you shadow boxing?");
                 return;
             }
         }
@@ -90,11 +127,16 @@ public class PublicModule : ModuleBase<SocketCommandContext>
     {
         await ReplyAsync("You will always put a space between your commands!" +
                          "\r\nTo use a command, try something like this! --> !Game account new" +
-                         "\r\n\r\naccount:" +
-                         "\r\n    new: Creates an account for said user." +
-                         "\r\n    showProfile: Shows the details of your account." +
-                         "\r\n    delete: Deletes the profile you own." +
-                         "\r\ndungeon:" +
-                         "\r\n    crawl: Moves you around in the dungeon.");
+                         "\r\n\r\n!Game" +
+                         "\r\n  account:" +
+                         "\r\n      new: Creates an account for said user." +
+                         "\r\n      showProfile: Shows the details of your account." +
+                         "\r\n      delete: Deletes the profile you own." +
+                         "\r\n  dungeon:" +
+                         "\r\n      crawl: Moves you around in the dungeon." +
+                         "\r\n      fight: Fights the monster you're currently boxing." +
+                         "\r\n  inventory:" +
+                         "\r\n      checkInv: Checks the weapons you have in your inventory." +
+                         "\r\n      swapHand: Swaps the weapon you are using for the one you want to swap with.");
     }
 }

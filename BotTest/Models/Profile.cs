8bc7d48bbb08740e00fd23408c9bee2a8687c9aa
Vincenzo Capra <vcapra71@gmail.com>
2024-03-25 10:41:33 -0400

    refactor: Moved defaults into model class

--- BotTest/Models/Profile.cs
+++ BotTest/Models/Profile.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TextCommandFramework.Models;
 
 public class Profile
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    public string ProfileId { get; set; }
-    public string Name { get; set; }
+    public string ProfileId { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public ulong DiscordId { get; set; }
-    public int Money { get; set; }
-    public int Level { get; set; }
-    public int Experience { get; set; }
-    public List<int> Inventory { get; set; }
-    public int Fight { get; set; }
-    public string CName { get; set; }
-    public int CExpGain { get; set; }
-    public int CHP { get; set; }
-    public int CDamage { get; set; }
+    public int Money { get; set; } = 0;
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+    public List<int> Inventory { get; set; } = new int[10].ToList();
+    public int Fight { get; set; } = -1;
+    public string CName { get; set; } = string.Empty;
+    public int CExpGain { get; set; } = 0;
+    public int CHP { get; set; } = 0;
+    public int CDamage { get; set; } = 0;
 }
